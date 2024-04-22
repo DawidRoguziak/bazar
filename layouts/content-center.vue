230aac9958288df8179c8500a5393676f0a12f40
dawidroguziak@gmail.com <dawidroguziak@gmail.com>
2024-04-22 21:15:14 +0200

    add products page

---
+++ layouts/content-center.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div>
+        <div class="flex justify-center p-2">
+            <div class="w-[clamp(300px,80%,1200px)]">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ pages/products/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: "content-center",
+});
+
+const { data, pending, error, refresh } = await useFetch(
+    "http://38.242.155.40:8000/api/categories",
+    {
+        onResponse({ request, response, options }) {
+            console.log("response", response._data);
+        },
+    }
+);
+</script>
+
+<template>
+    <div>
+        {{ data }}
+
+        <div>
+            <UiButton @click="refresh"> Refresh </UiButton>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
