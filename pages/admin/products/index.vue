dd3f7a5545fefe07d2a8264bbd368b0a389203e5
dawidroguziak@gmail.com <dawidroguziak@gmail.com>
2024-04-23 23:01:17 +0200

    add admin paths

---
+++ pages/admin/products/index.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: "content-center",
+});
+
+const runtimeConfig = useRuntimeConfig();
+
+const { data, pending, error, refresh } = await useFetch(
+    `${runtimeConfig.public.apiBase}/api/categories`
+);
+</script>
+
+<template>
+    <div>
+        {{ $t("welcome") }}
+        <div class="">
+            <UiButton size="sm" @click="refresh"> Small Fill </UiButton>
+            <UiButton @click="refresh"> Medium Fill </UiButton>
+            <UiButton size="lg" @click="refresh"> Large Fill </UiButton>
+            <UiButton size="icon" @click="refresh">
+                <span class="material-symbols-outlined"> settings </span>
+            </UiButton>
+        </div>
+        <div class="">
+            <UiButton variant="outline" size="sm" @click="refresh">
+                Small Fill
+            </UiButton>
+            <UiButton variant="outline" @click="refresh">
+                Medium Fill
+            </UiButton>
+            <UiButton variant="outline" size="lg" @click="refresh">
+                Large Fill
+            </UiButton>
+            <UiButton variant="outline" size="icon" @click="refresh">
+                <span class="material-symbols-outlined"> settings </span>
+            </UiButton>
+        </div>
+        <div class="">
+            <UiButton variant="ghost" size="sm" @click="refresh">
+                Small Fill
+            </UiButton>
+            <UiButton variant="ghost" @click="refresh"> Medium Fill </UiButton>
+            <UiButton variant="ghost" size="lg" @click="refresh">
+                Large Fill
+            </UiButton>
+            <UiButton variant="ghost" size="icon" @click="refresh">
+                <span class="material-symbols-outlined"> settings </span>
+            </UiButton>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
