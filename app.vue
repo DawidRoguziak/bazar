bd04a0d7504df94b9f66a2293818bdccd38aeaff
dawidroguziak@gmail.com <dawidroguziak@gmail.com>
2024-04-23 19:34:21 +0200

    // fix hydration problem

--- app.vue
+++ app.vue
@@ -1,7 +1,12 @@
 <script setup lang="ts">
 import { useLangStore } from "~/app-modules/language/useLangStore";
+import { ConfigProvider } from "radix-vue";
 
 const { getLanguage } = useLangStore();
+
+// https://www.radix-vue.com/utilities/config-provider#hydration-issue-vue-3-5
+// fix hydration problem
+const useIdFunction = () => useId();
 
 onMounted(() => {
     if (process.client) {
@@ -12,8 +17,10 @@ onMounted(() => {
 
 <template>
     <div>
-        <NuxtLayout>
-            <NuxtPage />
-        </NuxtLayout>
+        <ConfigProvider :use-id="useIdFunction">
+            <NuxtLayout>
+                <NuxtPage />
+            </NuxtLayout>
+        </ConfigProvider>
     </div>
 </template>
