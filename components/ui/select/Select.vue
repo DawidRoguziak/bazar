a115f43a5a9b6dd2d4c25fc078f2d42461f3d7e6
dawidroguziak@gmail.com <dawidroguziak@gmail.com>
2024-05-11 14:53:12 +0200

    add select to UI

---
+++ components/ui/select/Select.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import type { SelectRootEmits, SelectRootProps } from "radix-vue";
+import { SelectRoot, useForwardPropsEmits } from "radix-vue";
+
+interface UiSelectProps extends SelectRootProps {
+    name: string;
+}
+
+const props = defineProps<UiSelectProps>();
+const emits = defineEmits<SelectRootEmits>();
+
+const forwarded = useForwardPropsEmits(props, emits);
+</script>
+
+<template>
+    <div class="form-field">
+        <UiFormField v-slot="{ field }" :name="name">
+            <UiFormItem>
+                <SelectRoot v-bind="{ ...forwarded, ...field }">
+                    <slot />
+                </SelectRoot>
+            </UiFormItem>
+        </UiFormField>
+    </div>
+</template>
